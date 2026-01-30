f6d6956966ee035b0d3f0b5b9fe19d701e41e5be
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 01:12:32 +0300

    Update UserModel.jsx

--- client/src/components/UserModel.jsx
+++ client/src/components/UserModel.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {Check, X} from 'lucide-react';
 
-function UserModel(){
+function UserModel({isOpen, onClose}){
+
+    if(!isOpen) return null;
     return (
         <div className="fixed inset-0 bg-black/80 flex items-center justify-center p-4 z-50">
             <div className="bg-gray-900 rounded-lg shadow-2xl max-w-2xl w-full max-h-screen overflow-y-auto border border-gray-800">
                 <div className="flex justify-between items-center p-6 border-b border-gray-800">
                     <h2 className="text-2xl font-bold text-white">Add New User</h2>
-                    <button className="text-gray-400 hover:text-white transition-all">
+                    <button className="text-gray-400 hover:text-white transition-all" onClick={onClose} >
                         <X size={24} />
                     </button>
                 </div>
