43d7d2c7c8ebc4f0552c1a1f9d44ccac79355351
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 02:10:41 +0300

    ui editing

--- client/src/components/StatsCard.jsx
+++ client/src/components/StatsCard.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import {User} from "lucide-react";
+// import {User} from "lucide-react";
 
-function StatsCard(){
+function StatsCard({title, value, icon, bgIcon="bg-gray-700", iconColor="text-white", gradient="from-gray-900 to-gray-800",
+    description="", })
+{
     return ( 
-        <div className={`rounded-lg shadow-lg p-6 border border-gray-800 transform
-        hover:scale-105 transition-all bg-red-200`}>
+        <div className={`rounded-lg shadow-lg p-6 border bg-gradient-to-r ${gradient} border-gray-800 transform
+        hover:scale-105 transition-all`}>
             <div className="flex justify-between items-start">
                 <div>
-                <p className="text-gray-300 text-sm font-medium">Title</p>
-                <p className="text-3xl font-bold mt-2">Number</p>
+                <p className="text-gray-300 text-sm font-medium">{title}</p>
+                <p className="text-3xl font-bold mt-2" style={{color: value.color || "white"}}>{value.number}</p>
 
-                <p className="text-gray-400 text-sm mt-1">Description</p>
+                {description && ( <p className="text-gray-400 text-sm mt-1">{description}</p> )}
                 </div>
                 {/* Icon */}
-                <div className={`p-3 rounded-lg flex items-center justify-center`}>
-                    <User />
+                <div className={`p-3 rounded-lg flex items-center justify-center ${bgIcon}`}>
+                    {React.cloneElement(icon, {size: 22, className: iconColor})}
+                    {/* <User /> */}
                 </div>
             </div>
         </div>
