fcb3483327a074f80e10a104236a7ec6c7411340
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 04:55:33 +0300

    map method is used for listing users and general table ui

--- client/src/components/UserTable.jsx
+++ client/src/components/UserTable.jsx
@@ -60,8 +60,14 @@ function UserTable({users, onEdit, onDelete, currentPage, totalPages, onPageChan
                         {[...Array(totalPages)].map((_, i) => {
                             const p = i + 1;
                             if(p === 1 || p === totalPages || (p >= currentPage - 1 && p <= currentPage + 1)){
-                                return <button className={`px-3 py-2 rounded-lg`}>{p}</button>;
+                                return ( 
+                                    <button className={`px-3 py-2 rounded-lg ${currentPage === p ? "bg-blue-300 text-gray-900 font-semibold" 
+                                    : "bg-gray-700 border-gray-600 text-gray-300 hover:bg-gray-600"}`} onClick={() => onPageChange(p)} >{p}</button> 
+                                );
+                            } else if(p === currentPage - 2 || p === currentPage + 2){
+                                return <span className="px-2 py-2 text-gray-500">....</span>;
                             }
+                            return null;
                         })}
                         <button className="flex items-center gap-1 px-3 py-2 bg-gray-700 border border-gray-600 text-gray-300 rounded-lg hover:bg-gray-600
                         disabled:opacity-50" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
