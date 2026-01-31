d1e5be10dd750819a2055c38c3574eb20054d92d
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 04:33:28 +0300

    map method is used for listing users and general table ui

--- client/src/components/UserTable.jsx
+++ client/src/components/UserTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {ChevronLeft, ChevronRight, Edit} from 'lucide-react';
 import {Trash2} from 'lucide-react';
 
-function UserTable(){
+function UserTable({users, onEdit, onDelete, currentPage, totalPages, onPageChange}){
     return ( 
         <div className="bg-gray-900 rounded-lg overflow-hidden border border-gray-800">
             <div className="overflow-x-auto">
@@ -19,46 +19,57 @@ function UserTable(){
                     </thead>
                 
                     <tbody className="divide-y divide-gray-800">
-                        <tr className="hover:bg-gray-800 transition-colors">
-                            <td className="px-6 py-4 text-sm text-white font-medium">User Name</td>
-                            <td className="px-6 py-4 text-sm text-white font-medium">Email</td>
-                            <td className="px-6 py-4 text-sm text-white font-medium">Phone</td>
-                            <td className={`px-3 py-1 rounded-full text-xs font-semibold`}>Active</td>
-                            <td className="px-6 py-4 text-sm text-gray-400">Date</td>
-                            <td className="px-6 py-4 text-center">
-                                <div className="flex justify-center gap-2">
-                                    <button className="flex items-center gap-1 px-3 py-1.5 text-sm bg-blue-300 text-gray-900 rounded-lg hover:bg-blue-400 
-                                    transition-all font-semibold">
-                                    <Edit size={14} /> Edit
-                                    </button>
-                                    <button className="flex items-center gap-1 px-3 py-1.5 text-sm bg-red-500 text-white rounded-lg hover:bg-red-400 
-                                    transition-all font-semibold">
-                                    <Trash2 size={14} /> Delete
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colSpan={6} className="text-center py-12 text-gray-400">No users found</td>
-                        </tr>
+                        {users.map((u, index) => (
+                            <tr className="hover:bg-gray-800 transition-colors">
+                                <td className="px-6 py-4 text-sm text-white font-medium">{u.name}</td>
+                                <td className="px-6 py-4 text-sm text-white font-medium">{u.email}</td>
+                                <td className="px-6 py-4 text-sm text-white font-medium">{u.phone}</td>
+                                <td className="px-6 py-4 text-sm"><span className={`px-3 py-1 rounded-full text-xs font-semibold ${u.status === "active" ? "bg-green-400 text-gray-900" : "bg-red-400 text-white"}`}>{" "} {u.status}</span></td>
+                                <td className="px-6 py-4 text-sm text-gray-400">{new Date(u.createdAt).toLocaleDateString()}</td>
+                                <td className="px-6 py-4 text-center">
+                                    <div className="flex justify-center gap-2">
+                                        <button className="flex items-center gap-1 px-3 py-1.5 text-sm bg-blue-300 text-gray-900 rounded-lg 
+                                        hover:bg-blue-400 transition-all font-semibold" onClick={() => onEdit(u)}>
+                                            <Edit size={14} /> Edit
+                                        </button>
+                                        <button className="flex items-center gap-1 px-3 py-1.5 text-sm bg-red-500 text-white rounded-lg hover:bg-red-400 
+                                        transition-all font-semibold" onClick={() => onDelete(u._id)}>
+                                            <Trash2 size={14} /> Delete
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                        {users.length === 0 && (
+                            <tr>
+                                <td colSpan={6} className="text-center py-12 text-gray-400">No users found</td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
             {/* Pagination */}
-            <div className="px-6 py-4 border-t border-gray-800 flex justify-between items-center bg-gray-800">
-                <div className="text-sm text-gray-400">Page 1 of 5</div>
-                <div className="flex gap-2">
-                    <button className="flex items-center gap-1 px-3 py-2 bg-gray-700 border border-gray-600 text-gray-300 rounded-lg hover:bg-gray-600
-                    disabled:opacity-50">
-                        <ChevronLeft size={16}/> Prev 
-                    </button>
-                    <button className={`px-3 py-2 rounded-lg`}>1</button>
-                    <button className="flex items-center gap-1 px-3 py-2 bg-gray-700 border border-gray-600 text-gray-300 rounded-lg hover:bg-gray-600
-                    disabled:opacity-50">
-                        Next <ChevronRight size={16}/> 
-                    </button>
+            {users.length > 0 && (
+                <div className="px-6 py-4 border-t border-gray-800 flex justify-between items-center bg-gray-800">
+                    <div className="text-sm text-gray-400">Page {currentPage} of {totalPages}</div>
+                    <div className="flex gap-2">
+                        <button className="flex items-center gap-1 px-3 py-2 bg-gray-700 border border-gray-600 text-gray-300 rounded-lg hover:bg-gray-600
+                        disabled:opacity-50" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+                            <ChevronLeft size={16}/> Prev 
+                        </button>
+                        {[...Array(totalPages)].map((_, i) => {
+                            const p = i + 1;
+                            if(p === 1 || p === totalPages || (p >= currentPage - 1 && p <= currentPage + 1)){
+                                return <button className={`px-3 py-2 rounded-lg`}>{p}</button>;
+                            }
+                        })}
+                        <button className="flex items-center gap-1 px-3 py-2 bg-gray-700 border border-gray-600 text-gray-300 rounded-lg hover:bg-gray-600
+                        disabled:opacity-50" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+                            Next <ChevronRight size={16}/> 
+                        </button>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
