c20aa0834fce0943e4f593897e0e20158462dcda
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 05:34:00 +0300

    some functions are edited for updating and adding user

--- client/src/components/UserModel.jsx
+++ client/src/components/UserModel.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import {Check, X} from 'lucide-react';
 
-function UserModel({isOpen, onClose}){
+function UserModel({isOpen, onClose, formData, setFormData, onSubmit, loading, status}){
 
     if(!isOpen) return null;
     return (
         <div className="fixed inset-0 bg-black/80 flex items-center justify-center p-4 z-50">
             <div className="bg-gray-900 rounded-lg shadow-2xl max-w-2xl w-full max-h-screen overflow-y-auto border border-gray-800">
                 <div className="flex justify-between items-center p-6 border-b border-gray-800">
-                    <h2 className="text-2xl font-bold text-white">Add New User</h2>
+                    <h2 className="text-2xl font-bold text-white">{formData._id ? "Edit User" : "Add New User"}</h2>
                     <button className="text-gray-400 hover:text-white transition-all" onClick={onClose} >
                         <X size={24} />
                     </button>
@@ -17,36 +17,41 @@ function UserModel({isOpen, onClose}){
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div>
                             <label className="block text-gray-300 font-medium mb-2">Name *</label>
-                            <input type="text" placeholder="John Deo" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 text-white
+                            <input type="text" value={formData.name} onChange={(e) => setFormData({...formData, name: e.target.value})} 
+                            placeholder="John Deo" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 text-white
                             placeholder-gray-500 rounded-lg focus:ring-2 focus:ring-blue-300 outline-none" />
                         </div>
                         <div>
                             <label className="block text-gray-300 font-medium mb-2">E-mail *</label>
-                            <input type="email" placeholder="john@example.com" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 
+                            <input type="email" value={formData.email} onChange={(e) => setFormData({...formData, email: e.target.value})} 
+                            placeholder="john@example.com" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 
                             text-white placeholder-gray-500 rounded-lg focus:ring-2 focus:ring-blue-300 outline-none" />
                         </div>
                         <div>
                             <label className="block text-gray-300 font-medium mb-2">Phone Number *</label>
-                            <input type="tel" placeholder="+1234567890" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 text-white
+                            <input type="tel" value={formData.phone} onChange={(e) => setFormData({...formData, phone: e.target.value})} 
+                            placeholder="+1234567890" className="w-full px-4 py-2.5 bg-gray-800 border border-gray-700 text-white
                             placeholder-gray-500 rounded-lg focus:ring-2 focus:ring-blue-300 outline-none" />
                         </div>
                         <div>
                             <label className="block text-gray-300 font-medium mb-2">Stats *</label>
                             <select className="w-full px-4 py-2.5 bg-gray-800 border-gray-700 text-white rounded-lg focus:ring-2 focus:ring-blue-300
-                            focus:border-blue-300 outline-none">
-                                <option>Active</option>
+                            focus:border-blue-300 outline-none" value={formData.status} onChange={(e) => setFormData({...formData, status: e.target.value})}>
+                                {status.map((status) => (
+                                    <option value={status}>{status}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
                     <div className="flex gap-3 mt-6">
                         <button className="flex-1 px-4 py-2.5 bg-gray-800 border border-gray-700 text-gray-300 rounded-lg hover:bg-gray-700
-                        transition-all">
+                        transition-all" onClick={onClose}>
                             Cancel
                         </button>
                         <button className="flex-1 flex items-center justify-center px-4 py-2.5 border border-gray-700 bg-blue-300 text-gray-900 rounded-lg hover:bg-blue-200
-                        transition-all">
+                        transition-all" onClick={onSubmit} disabled={loading}>
                             <Check size={20} />
-                            Add User
+                            {loading ? "Saving..." : formData._id ? "Update User" : "Add User"}
                         </button>
                     </div>
                 </div>
