c21fcf65cae2a16dcb0113c9c5235fb73a500d6d
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-01-31 03:11:37 +0300

    updated some functions for searching

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import {Search, X} from "lucide-react";
 
-function SearchBar(){
-    return <div className="bg-gray-900 rounded-lg shadow-lg p-4 border border-gray-800 flex flex-col
-    md:flex-row md:items-center md:justify-between gap-4">
-        {/* Search Input */}
-        <div className="relative flex-1">
-            <Search size={16} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 pointer-events-none"/>
-            <input type="text" placeholder="Search by name, e-mail, phone or status..." className="w-full pl-10 pr-10 py-2.5
-            bg-gray-800 border border-gray-700 text-white placeholder-gray-500 rounded-lg focus:ring-2 focus:ring-blue-300
-            focus:border-blue-300 outline-none"/>
-            <button className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-white hover:bg-gray-700
-            p-1 rounded-full transition-all">
-                <X size={14} />
-            </button>
-        </div>
+function SearchBar({value, onChange, onClear, itemsPerPage, onItemsPerPageChange, currentPage, totalUsers}){
 
-        {/* Rows per page & info */}
-        <div className="flex items-center gap-4">
-            <span className="text-sm text-gray-400">Showing 1 to 5 of 10 Users</span>
-            <div className="flex items-center gap-2">
-                <label className="text-sm text-gray-400">Rows</label>
-                <select className="px-3 py-1.5 bg-gray-800 border border-gray-700 text-white rounded-lg focus:ring-2
-                focus:ring-blue-300 focus:border-blue-200 outline-none text-sm">
-                    <option>5</option>
-                    <option>10</option>
-                    <option>25</option>
-                    <option>50</option>
-                </select>
+    const startUser = totalUsers === 0 ? 0: (currentPage - 1) * itemsPerPage + 1;
+    const endUser = Math.min(currentPage * itemsPerPage, totalUsers);
+
+    return (
+        <div className="bg-gray-900 rounded-lg shadow-lg p-4 border border-gray-800 flex flex-col
+        md:flex-row md:items-center md:justify-between gap-4">
+            {/* Search Input */}
+            <div className="relative flex-1">
+                <Search size={16} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 pointer-events-none"/>
+                <input type="text" value={value} onChange={(e) => onChange(e.target.value)} placeholder="Search by name, e-mail, phone or status..." 
+                className="w-full pl-10 pr-10 py-2.5 bg-gray-800 border border-gray-700 text-white placeholder-gray-500 rounded-lg focus:ring-2 focus:ring-blue-300
+                focus:border-blue-300 outline-none"/>
+                {value && (
+                    <button className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-white hover:bg-gray-700
+                    p-1 rounded-full transition-all">
+                        <X size={14} />
+                    </button>
+                )}
+            </div>
+
+            {/* Rows per page & info */}
+            <div className="flex items-center gap-4">
+                <span className="text-sm text-gray-400">Showing {startUser} to {endUser} of {totalUsers} Users</span>
+                <div className="flex items-center gap-2">
+                    <label className="text-sm text-gray-400">Rows</label>
+                    <select className="px-3 py-1.5 bg-gray-800 border border-gray-700 text-white rounded-lg focus:ring-2
+                    focus:ring-blue-300 focus:border-blue-200 outline-none text-sm" onChange={(e) => onItemsPerPageChange(Number(e.target.value))}>
+                        <option value={5}>5</option>
+                        <option value={10}>10</option>
+                        <option value={25}>25</option>
+                        <option value={50}>50</option>
+                    </select>
+                </div>
             </div>
         </div>
-    </div>;
+    );
 }
 
 export default SearchBar;
