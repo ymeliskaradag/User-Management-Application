808cda59440fc40d2a011c3476fdfb6d132d090e
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2026-02-08 01:12:36 +0300

    configures for Render

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ mongoose.connect(process.env.MONGODB_URI.replace("<DB_PASSWORD>", process.env.DA
     console.log("MongoDB Connection Error", error);
 });
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
+
 app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}...`);
 })
